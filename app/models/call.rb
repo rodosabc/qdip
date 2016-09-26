4c9fa85ce4942a6af0e6739d49b82df68f0b1fd6
rodosabc <rodosabc@gmail.com>
2016-09-27 02:23:38 +0300

    Some changes in model, views and locale

--- app/models/call.rb
+++ app/models/call.rb
@@ -1,3 +1,2 @@
 class Call < ApplicationRecord
-  validates :phone_number, presence: true
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,7 @@ class Category < ApplicationRecord
 
   has_attached_file :photo, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :photo, content_type: /\Aimage\/.*\z/
+
+  validates :name, presence: true
+  validates :photo, presence: true
 end
